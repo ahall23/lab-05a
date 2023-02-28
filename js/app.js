6dd8da0cdff80400168ca1265aafe7bda43d9714
Alexia Hall <alexiahall306@icloud.com>
2023-02-28 13:55:41 -0600

    yay1
    i had to change sum variable to summ

--- js/app.js
+++ js/app.js
@@ -59,7 +59,7 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function sumAndMultiply(a, b, c) {
   let summ = sum(a, b)[0];
-  let sum2 = sum(sum, c)[0];
+  let sum2 = sum(summ, c)[0];
 
   let product = multiply(a, b)[0];
   let product2 = multiply(product, c)[0];
@@ -111,7 +111,7 @@ function sumArray(sumArr) {
 
 // Here is the test for sumArray(); uncomment it to run it
 
-//testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -140,7 +140,7 @@ function multiplyArray(multArr) {
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-//testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop.
 
